90c26994e608ccdf54a9e68b163745ab84c277f8
gamicolo <guillermoamicolo@gmail.com>
2021-06-02 22:30:51 -0300

    feat[ci]:remove commented code

--- test/post_client.py
+++ test/post_client.py
@@ -10,7 +10,6 @@ BASE = "http://127.0.0.1:5000/"
 utc_str = datetime.utcnow().replace(tzinfo=pytz.timezone('UTC')).isoformat('T')
 
 print(utc_str)
-#response = requests.post(BASE + "transactions", {'amount': '1', 'timestamp': '2018-07-17T09:59:51.312Z'})
 response = requests.post(BASE + "transactions", {'amount': str(int(500*random.random())), 'timestamp': utc_str})
 
 #invalid request
