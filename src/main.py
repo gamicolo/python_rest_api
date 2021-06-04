c178dfe47d21b9e0a66e79d51ecaf71edda2111e
gamicolo <guillermoamicolo@gmail.com>
2021-06-03 22:55:15 -0300

    feat[ci]:add safety to shared memory data

--- src/main.py
+++ src/main.py
@@ -7,16 +7,18 @@ import calendar
 import pytz
 import math
 import logging
+from multiprocessing import Manager, Lock
 
 #logging.basicConfig(level=logging.DEBUG, filename='real_time_stats.log', filemode='w', format='%(asctime)s %(name)s %(levelname)s %(message)s')
-logging.basicConfig(level=logging.ERROR, format='%(asctime)s %(name)s %(levelname)s %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)s %(levelname)s %(message)s')
 
 logger = logging.getLogger('real_time_stats')
 
 app = Flask(__name__)
 api = Api(app)
 
-transactions={}
+shared_transactions = Manager().dict()
+lock = Lock()
 
 transactions_post_args = reqparse.RequestParser()
 
@@ -31,35 +33,13 @@ def iso2unix(timestamp):
     parsed_t = dp.parse(timestamp)
     return calendar.timegm(parsed_t.timetuple())
 
-def get_transactions(transactions):
-    #make a local copy of the transactions dict to iterate over and not be affected when remove values older than 60 secs
-    local_transactions = dict(transactions)
-    result = []
-    current_utimestamp = get_current_timestamp_utc()
-    logger.debug('The current utc unix timestamp is: %s' % current_utimestamp)
-    for k in local_transactions.keys():
-        utimestamp = local_transactions[k]['utimestamp']
-        logger.debug('The unix timestamp in transactions dict is: %s' % utimestamp)
-        if (current_utimestamp - utimestamp) < 60:
-            result.append({'amount':local_transactions[k]['amount'],'timestamp': local_transactions[k]['timestamp']})
-        else:
-            #remove old values from the original and not the copy
-            logger.info('Removing iso timestamp key %s from transactions dict (older than 60 sec)' % k)
-            transactions.pop(k)
-    return result
-
-def post_transaction(transactions):
-
-    args = transactions_post_args.parse_args()
-    
-    timestamp = args['timestamp']
-    amount = args['amount']
+def post_transaction(transactions,amount,timestamp,lock):
 
     #422 body fields cannot be parsed (non exists or invalid type)
-    if not(args['amount']) or not(type(args['amount']) == str):
+    if not amount or not(type(amount) == str):
         logger.error('amount is None or not type str')
         return 422
-    if not(args['timestamp']) or not(type(args['amount']) == str):
+    if not timestamp or not(type(amount) == str):
         logger.error('timestamp is None or not type str')
         return 422
 
@@ -67,46 +47,68 @@ def post_transaction(transactions):
 
     delta = get_current_timestamp_utc() - unix
 
-    #422 transaccion con TS mayor al UTC actual
+    logger.debug('The difference between the iso timestamp from transaction and current utc timestamp is: %s' % delta)
+
+    #422 transaction with TS greather than current UTC timestamp
     if (delta < 0):
         return 422
 
-    #400 json invalido
+    #400 invalid json
     #TODO: implement 
 
     #204 transactions older than 60 secs
     if (delta > 60):
         return 204
 
-    #201 exito
-    transactions.update({timestamp:{'amount':amount,'timestamp':timestamp,'utimestamp':unix}})
-    logger.debug('Adding iso timestamp key %s to transactions dict' % timestamp)
+    #201 success
+    with lock:
+        transactions.update({timestamp:{'amount':amount,'timestamp':timestamp,'utimestamp':unix}})
+        logger.debug('Adding iso timestamp key %s to transactions dict' % timestamp)
     return 201
 
-def get_statistics(transactions):
-    local_transactions = dict(transactions)
+def get_transactions(transactions,lock):
+    result = []
+    current_utimestamp = get_current_timestamp_utc()
+    logger.debug('The current utc unix timestamp is: %s' % current_utimestamp)
+    with lock:
+        #make a local copy of the transactions dict to iterate over and not be affected when remove values older than 60 secs
+        local_transactions = dict(transactions)
+        for k in local_transactions.keys():
+            utimestamp = local_transactions[k]['utimestamp']
+            logger.debug('The unix timestamp in transactions dict is: %s' % utimestamp)
+            if (current_utimestamp - utimestamp) < 60:
+                result.append({'amount':local_transactions[k]['amount'],'timestamp': local_transactions[k]['timestamp']})
+            else:
+                #remove old values from the original and not the copy
+                logger.info('Removing iso timestamp key %s from transactions dict (older than 60 sec)' % k)
+                transactions.pop(k)
+    return result
+
+def get_statistics(transactions,lock):
     statistics = {'sum': 0, 'avg': 0, 'max': 0, 'min': 0, 'p90': 0, 'count': 0}
     current_utimestamp = get_current_timestamp_utc()
     logger.debug('The current utc unix timestamp is: %s' % current_utimestamp)
     amount_list=[]
-    for k in local_transactions.keys():
-        utimestamp = local_transactions[k]['utimestamp']
-        logger.debug('The unix timestamp in transactions dict is: %s' % utimestamp)
+    with lock:
+        local_transactions = dict(transactions)
+        for k in local_transactions.keys():
+            utimestamp = local_transactions[k]['utimestamp']
+            logger.debug('The unix timestamp in transactions dict is: %s' % utimestamp)
 
-        if (current_utimestamp - utimestamp) < 60:
-            statistics['sum'] += int(local_transactions[k]['amount'])
-            if (int(local_transactions[k]['amount']) > statistics['max']):
-                statistics['max']=int(local_transactions[k]['amount'])
-            if (statistics['min'] == 0):
-                statistics['min']=int(local_transactions[k]['amount'])
-            if (int(local_transactions[k]['amount']) < statistics['min']):
-                statistics['min']=int(local_transactions[k]['amount'])
-            statistics['count'] += 1
-            amount_list.append(int(local_transactions[k]['amount']))
-        else:
-            #remove old values from the original and not the copy
-            logger.info('Removing iso timestamp key %s from transactions dict (older than 60 sec)' % k)
-            transactions.pop(k)
+            if (current_utimestamp - utimestamp) < 60:
+                statistics['sum'] += int(local_transactions[k]['amount'])
+                if (int(local_transactions[k]['amount']) > statistics['max']):
+                    statistics['max']=int(local_transactions[k]['amount'])
+                if (statistics['min'] == 0):
+                    statistics['min']=int(local_transactions[k]['amount'])
+                if (int(local_transactions[k]['amount']) < statistics['min']):
+                    statistics['min']=int(local_transactions[k]['amount'])
+                statistics['count'] += 1
+                amount_list.append(int(local_transactions[k]['amount']))
+            else:
+                #remove old values from the original and not the copy
+                logger.info('Removing iso timestamp key %s from transactions dict (older than 60 sec)' % k)
+                transactions.pop(k)
     if (statistics['count'] > 0):
         statistics['avg'] = statistics['sum']/statistics['count']
     if amount_list:
@@ -124,30 +126,27 @@ def get_percentile(percentile, samples = []):
 
 class Transactions(Resource):
 
-    def get(self):
-
-        global transactions
-        result = get_transactions(transactions)
-        return result,200
-
     def post(self):
 
-        global transactions
-        r_code = post_transaction(transactions)
+        args = transactions_post_args.parse_args()
+        r_code = post_transaction(shared_transactions,args['amount'],args['timestamp'],lock)
         return {},r_code
+
+    def get(self):
+
+        result = get_transactions(shared_transactions,lock)
+        return result,200
 
     def delete(self):
 
-        global transactions
-        transactions.clear()
+        shared_transactions.clear()
         return 204
 
 class Statistics(Resource):
 
     def get(self):
 
-        global transactions
-        result = get_statistics(transactions)
+        result = get_statistics(shared_transactions,lock)
         return result,200
 
 api.add_resource(Transactions, "/transactions")
